405d51338b8f727bd94f088313c9691e86026367
mexalon <75248839+mexalon@users.noreply.github.com>
2021-07-23 16:22:24 +0300

    db url fixed 2

--- app/config.py
+++ app/config.py
@@ -2,6 +2,6 @@
 import os
 
 DATABASE_URL = os.getenv(
-    'POSTGRE_URL',
+    'DATABASE_URL',
     'postgresql://flask_hw:flask_hw@127.0.0.1:5432/flask_hw')
 SALT = 'ksfh@#$$5Kgfkорплапл*&^*&7fy374'
