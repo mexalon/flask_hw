2ce2fa28a46d090f1fc2c452ea485b69b1f2d8fa
Baryshnikov Nikolay <nabarysh@gmail.com>
2021-08-07 19:22:51 +0300

    one more time 2

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ DATABASE_URL = os.getenv(
     'DATABASE_URL',
     'postgresql://flask_hw:flask_hw@127.0.0.1:5432/flask_hw')
 
-REDIS_URL = os.getenv('REDIS_URL', 'redis://127.0.0.1:6379')
+REDIS_URL = os.getenv('REDIS_TLS_URL', 'redis://127.0.0.1:6379')
 
 SALT = os.getenv('SALT', 'ksfh@#$$5Kgfkорплапл*&^*&7fy374')
 
